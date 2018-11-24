9b615f0a1585c9f469234b9bce244b4423072b27
AzarovDN <azarovdn@icloud.com>
2018-11-24 21:12:58 +1000

    Решил первое задание

--- find_procedure.py
+++ find_procedure.py
@@ -38,8 +38,57 @@
 import os
 
 migrations = 'Migrations'
-current_dir = os.path.dirname(os.path.abspath(__file__))
+# current_dir = os.path.dirname(os.path.abspath(__file__))
 
-if __name__ == '__main__':
-    # ваша логика
-    pass
+print('-' * 40)
+
+def find_file(files_list = []):
+    n = 0
+    text = input('Введите текст для поиска:')
+    if files_list == []:
+        os.chdir('/Users/azarovdn/Desktop/Netology/03_Python/08_Work_whth_path/Migrations')
+        for files in os.listdir("/Users/azarovdn/Desktop/Netology/03_Python/08_Work_whth_path/Migrations/"):
+            file_inside = True
+            if files.endswith(".sql"):
+                with open(files) as file:
+                    for line in file:
+                        if text in line and file_inside is True:
+                            files_list.append(files)
+                            n += 1
+                            file_inside = False
+
+    else:
+        files_list_doubler = []
+        for files in files_list:
+            with open(files) as file:
+                file_inside = True
+                for line in file:
+                    if text in line and file_inside is True:
+                        files_list_doubler.append(files)
+                        file_inside = False
+                        n += 1
+        files_list = files_list_doubler
+
+    if files_list == []:
+        print('Файлы не обнаружены')
+        return
+    elif len(files_list) > 10:
+        print('... большой список файлов ...')
+        print('Всего: {}'.format(len(files_list)))
+        find_file(files_list)
+    elif len(files_list) > 1:
+        print('Всего: {}'.format(len(files_list)))
+        for file in files_list:
+            print('{}/{}'.format(migrations,file))
+        find_file(files_list)
+    elif len(files_list) == 1:
+        print('{}/{}'.format(migrations,files_list[0]))
+        return
+
+
+find_file()
+
+
+
+# if __name__ == '__main__':
+#     passv
